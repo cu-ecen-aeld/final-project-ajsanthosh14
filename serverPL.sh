a7dbac5a1725cf2c3899df75e6b4856c815dba5d
ajsanthosh14 <ajsanthosh12@gmail.com>
2022-04-03 01:08:02 -0600

    UDP unicast working

--- serverPL.sh
+++ serverPL.sh
@@ -5,14 +5,36 @@
 #Reference: gstreamer documentation
 #Link: https://gstreamer.freedesktop.org/documentation/tools/gst-launch.html?gi-language=c
 
-IP=$(ip route get 8.8.8.8 | grep -oP 'src \K[^ ]+')
 
-echo BRADCASTING with IP address: $IP
+#Video source
+VSRC=video_samples/sample.mp4
 
-#Broadcasts only video portion of MPEG video file
-gst-launch-1.0 -v filesrc location =video_samples/sample.mp4 ! decodebin ! x264enc ! rtph264pay ! udpsink host=$IP port=5000 
 
 # Get public ip 
 #echo $(dig +short myip.opendns.com @resolver1.opendns.com)
+
+
+
+#Broadcasting only video portion of MPEG video file
+
+: '
+IP=$(ip route get 8.8.8.8 | grep -oP 'src \K[^ ]+')
+
+echo BRADCASTING IP IP address: $IP
+
+gst-launch-1.0 -v filesrc location=$VSRC  ! decodebin ! x264enc ! rtph264pay ! udpsink host=$IP port=5000 
+ '
+
+# Audio+Video broadcasting over UDP
+
+TARGET_IP=10.0.0.83
+
+echo BRADCASTING IP IP address: $TARGET_IP
+
+
+gst-launch-1.0 -v filesrc location=$VSRC ! \
+       	decodebin name=dec ! videoconvert ! x264enc ! video/x-h264 ! mpegtsmux name=mux ! queue ! udpsink host=$TARGET_IP port=5000 sync=true \
+       	dec. ! queue ! audioconvert ! voaacenc ! audio/mpeg ! queue ! mux.
+
 
 
