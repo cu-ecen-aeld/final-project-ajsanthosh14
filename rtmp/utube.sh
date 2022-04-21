7c9b9ac106c5cc3f55c02a193c3f72a93e332146
Demo User <debian@beaglebone.localdomain>
2022-04-21 01:16:15 +0000

    rtmp test utube

--- rtmp/utube.sh
+++ rtmp/utube.sh
@@ -1,3 +1,10 @@
-# stream video to youtube live streaming
+#!/bin/bash 
+#Script to stream video to youtube live streaming using RTMP
+#Author: Santhosh
 
-gst-launch-1.0 -v videotestsrc ! decodebin ! videoconvert ! x264enc ! video/x-h264 ! h264parse ! flvmux streamable=true name=mux ! rtmpsink location="rtmp://a.rtmp.youtube.com/live2/7t9h-dwt1-9zfe-pgtq-9411" audiotestsrc ! voaacenc bitrate=128000 ! mux.
+# tcp pipeline
+
+gst-launch-1.0 -v videotestsrc ! decodebin ! videoconvert \
+ ! x264enc ! video/x-h264 ! h264parse ! flvmux streamable=true name=mux \
+ ! rtmpsink location="rtmp://a.rtmp.youtube.com/live2/7t9h-dwt1-9zfe-pgtq-9411" audiotestsrc  \
+ ! voaacenc bitrate=128000 ! mux.
